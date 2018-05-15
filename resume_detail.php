cde27ea4cdac9bc00a6ef2584978f3878edd7048
elephant2 <2224168716@qq.com>
2018-05-15 10:28:10 +0800

    renew bootstrap4 style for resume_detail.php

--- resume_detail.php
+++ resume_detail.php
@@ -30,24 +30,36 @@ catch (Exception $exception)
 include 'lib/Parsedown.php';
 $md = new Parsedown();
 ?>
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang="zh-cn">
 <head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title><?=$resume['title'];?></title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://bootswatch.com/4/sketchy/bootstrap.min.css" >
+
     <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-    <script src="main.js"></script>
-    <!--引入CDN的js库，引入大站点的会避免加载js直接用缓存   -->
-    <script src="http://lib.sinaapp.com/js/jquery/3.1.0/jquery-3.1.0.min.js"></script>
+
+    <title><?=$resume['title'];?></title>
 </head>
 <body>
-    <div class="container">
-        <div class="content">
-            <?=$md->text($resume['content']);?>
-        </div>
+<div class="alert alert-info" role="alert">
+<!--    <h4 class="alert-heading">Well done!</h4>-->
+    <p><?=$md->text($resume['content']);?></p>
+    <hr>
+    <p class="mb-0">感谢您的浏览.</p>
+</div>
 
-    </div>
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+<script src="main.js"></script>
+<!--引入CDN的js库，引入大站点的会避免加载js直接用缓存   -->
+<script src="http://lib.sinaapp.com/js/jquery/3.1.0/jquery-3.1.0.min.js"></script>
 </body>
 </html>
